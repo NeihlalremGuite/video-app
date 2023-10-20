4ca796f9838dda869ef4f9dc49cb9a8f2f1d1a50
Neihlalrem Guite <neihlalremguite@Pro.local>
2023-10-21 00:10:12 +0530

    final

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 
-let cors = require("cors");
+import cors from "cors";
 
 const app = express();
 app.use(cors());
